a436005862ac0dfd9681baa9c4a28e50ccf0a473
mzakocs <mzakocs@gmail.com>
2022-04-28 13:03:08 -0700

    oops

--- public/humanevent/mySketch.js
+++ public/humanevent/mySketch.js
@@ -9,7 +9,7 @@ var count = 0;
 
 // Loads the sketch image
 function preload() {
-  imgs[0] = loadImage("/eng105/mitch.png");
+  imgs[0] = loadImage("/humanevent/mitch.png");
 }
 
 // Sets up the canvas
